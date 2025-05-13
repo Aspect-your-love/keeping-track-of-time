00371934118a1ab05642aedc5a08d909d8ef93e3
Aspect-your-love <kras.developer@yandex.ru>
2025-05-13 11:57:03 +0700

    Add backend and frontend project, documentation folder

---
+++ keep-time-tracking-backend/src/main/java/net/aspect/time_tracking/KeepTimeTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package net.aspect.time_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepTimeTrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepTimeTrackingBackendApplication.class, args);
+	}
+
+}
